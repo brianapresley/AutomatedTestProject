4a3056023e8f2c0167212194cc0710bd44948661
Briana Presley <presley.briana@gmail.com>
2019-08-12 10:49:42 -0400

    Add project files.

---
+++ AutomatedTestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutomatedTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
